0729fd3b73d4891fd41344fc381783d5813b391d
Mirelys Carcana Barbosa <mirelys_carcana_barbosa@brown.edu>
2025-08-11 22:00:12 -0500

    adding 1D hyper test cases, noted in toolchain that these are currently broken

--- examples/1D_hyper_expansion_weak/case.py
+++ examples/1D_hyper_expansion_weak/case.py
@@ -8,18 +8,18 @@ dx      = 1./(1.*(Nx+1))
 
 Tend    = 64E-06
 Nt      = 4000
-mydt    = Tend/(1.E+00*Nt)
-#print(mydt)
+mydt    = Tend/(1.*Nt)
+
 # Configuring case dictionary
 print(json.dumps({
                     # Logistics ================================================                 
                     'run_time_info'                : 'T',                       
-                    # ==========================================================                                                                                
+                    # ==========================================================
                     # Computational Domain Parameters ==========================
                     'x_domain%beg'                 : 0.E+00,                    
                     'x_domain%end'                 : 1.E+00,                    
-              #      'y_domain%beg'                 : 0.E+00,
-              #      'y_domain%end'                 : 0.002,
+#                    'y_domain%beg'                 : 0.E+00,
+#                    'y_domain%end'                 : 0.002,
                     'm'                            : Nx,                        
                     'n'                            : 0,                         
                     'p'                            : 0,                         
@@ -27,52 +27,51 @@ print(json.dumps({
                     't_step_start'                 : 0,                         
                     't_step_stop'                  : int(Nt),
                     't_step_save'                  : int(Nt/200),
-		    # ==========================================================                     
+		    # ==========================================================                    
                     # Simulation Algorithm Parameters ==========================
                     'num_patches'                  : 2,                        
                     'model_eqns'                   : 3,                        
                     'alt_soundspeed'               : 'F',                      
-                    'num_fluids'                   : 1,
-        	    'mpp_lim'                      : 'F',                        	    
-                    'mixture_err'                  : 'F',
-	            'time_stepper'                 : 3,                                     
+                    'num_fluids'                   : 1, 
+         	    'mpp_lim'                      : 'F',                      
+		    'mixture_err'                  : 'F',                      
+	            'time_stepper'                 : 3, 
                     'weno_order'                   : 5,                        
                     'weno_eps'                     : 1.E-16,
-	            'weno_Re_flux'                 : 'F',
-                    'weno_avg'                     : 'F',
+	            'weno_Re_flux'                 : 'F',  
+    	            'weno_avg'                     : 'F',
                     'mapped_weno'                  : 'F',                     
                     'null_weights'                 : 'F',                      
-                    'mp_weno'                      : 'T',
-                    'riemann_solver'               : 2,                        
+                    'mp_weno'                      : 'T',                      
+	            'riemann_solver'               : 2,                        
                     'wave_speeds'                  : 1,                        
                     'avg_state'                    : 2,                        
                     'bc_x%beg'                     : -3,                       
                     'bc_x%end'                     : -3,                       
-                #    'bc_y%beg'                     : -3,                       
-                #    'bc_y%end'                     : -3,                       
+                    #'bc_y%beg'                     : -3,                       
+                    #'bc_y%end'                     : -3,                       
                     # ==========================================================
 
                     # Turning on Hyperelasticity ================================
-                    'hyperelasticity'               : 'T',                     
+                    'hyperelasticity'               : 'T',                      
                     'hyper_model'                   : 1,
-                    'fd_order'                      : 1, 
+                    'fd_order'                      : 1,
                     # ==========================================================
                                                                                
                     # Formatted Database Files Structure Parameters ============
                     'format'                       : 1,                        
                     'precision'                    : 2,                        
-                    'prim_vars_wrt'                :'T', 
-                    'tau_wrt'                      :'T',                      
+                    'prim_vars_wrt'                :'T',
         	    'parallel_io'                  :'F',                       
-		    # ==========================================================                                                                        
+		    # ==========================================================                                       
 		    # Patch 1 L ================================================
                     'patch_icpp(1)%geometry'       : 1,                     
                     'patch_icpp(1)%x_centroid'     : 0.25,
-                #    'patch_icpp(1)%y_centroid'     : 0.001,                   
+ #                   'patch_icpp(1)%y_centroid'     : 0.001,                   
                     'patch_icpp(1)%length_x'       : 0.5,                    
-                #    'patch_icpp(1)%length_y'       : 0.002,
-                    'patch_icpp(1)%vel(1)'         : 10,  
-               #     'patch_icpp(1)%vel(2)'         : 0, 
+ #                   'patch_icpp(1)%length_y'       : 0.002,
+                    'patch_icpp(1)%vel(1)'         : -50,  
+ #                   'patch_icpp(1)%vel(2)'         : 100*0, 
                     'patch_icpp(1)%pres'           : 1.E+5,                    
                     'patch_icpp(1)%alpha_rho(1)'   : 1000,                    
                     'patch_icpp(1)%alpha(1)'       : 1.,                
@@ -81,11 +80,11 @@ print(json.dumps({
                     # Patch 2 R ================================================
                     'patch_icpp(2)%geometry'       : 1,                     
                     'patch_icpp(2)%x_centroid'     : 0.75,                  
-               #     'patch_icpp(2)%y_centroid'     : 0.001,
+#                    'patch_icpp(2)%y_centroid'     : 0.001,
                     'patch_icpp(2)%length_x'       : 0.5,  
-               #     'patch_icpp(2)%length_y'       : 0.002,                 
-                    'patch_icpp(2)%vel(1)'         : -10, #10, 
-               #     'patch_icpp(2)%vel(2)'         : 0,                   
+#                    'patch_icpp(2)%length_y'       : 0.002,                 
+                    'patch_icpp(2)%vel(1)'         : 50, 
+#                    'patch_icpp(2)%vel(2)'         : -100*0,                   
                     'patch_icpp(2)%pres'           : 1.E+05,               
                     'patch_icpp(2)%alpha_rho(1)'   : 1000,                 
                     'patch_icpp(2)%alpha(1)'       : 1.,                
@@ -94,7 +93,7 @@ print(json.dumps({
                     # Fluids Physical Parameters ===============================
                     'fluid_pp(1)%gamma'            : 1.E+00/(4.4E+00-1.E+00),   
                     'fluid_pp(1)%pi_inf'           : 4.4E+00*6.E+08/(4.4E+00 - 1.E+00), 
-                    'fluid_pp(1)%G'                : 1.E+010,                       
+                    'fluid_pp(1)%G'                : 1E+010,                       
 	            # ==========================================================
 }))
 # ==============================================================================
