a61e5075959104f7a07bc997513f4f9bc6d62963
Mauro Rodriguez <mauro_rodriguez@brown.edu>
2025-12-31 17:57:32 -0500

    testing base simulations first

--- toolchain/mfc/test/cases.py
+++ toolchain/mfc/test/cases.py
@@ -1034,7 +1034,7 @@ def list_cases() -> typing.List[TestCaseBuilder]:
                            "2D_backward_facing_step",
                            "2D_forward_facing_step",
                            "2D_whale_bubble_annulus_mod","2D_tree3rings","1D_convergence",
-                           "2D_treering_uniform",
+                           "2D_treering_uniform","2D_treering_cluster",
                            "3D_IGR_33jet"]
             if path in casesToSkip:
                 continue
